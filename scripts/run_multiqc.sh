d7d71da6f5ba19259be1516cd8521f8b6a06b74e
soom12 <soo.m@northeastern.edu>
2024-04-04 17:38:24 -0400

    fixed multiqc conda

--- scripts/run_multiqc.sh
+++ scripts/run_multiqc.sh
@@ -2,6 +2,8 @@
 
 source ./config.cfg
 
+module load anaconda3/2021.05
+eval "$(conda shell.bash hook)"
 conda activate /work/geisingerlab/conda_env/multiQC
 
-multiqc $BASE_DIR --ignore
+multiqc $BASE_DIR --ignore data/ --ignore software/ --ignore ref/
